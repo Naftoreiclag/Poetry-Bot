a8d9ea1492ff04cf4abc0a53169b5acdedbc0780
Naftoreiclag <naftoreiclag@gmail.com>
2013-12-09 22:52:35 -0800

    Added shuffled word list

--- PoetryBot.java
+++ PoetryBot.java
@@ -7,21 +7,32 @@
 package foo;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.BitSet;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PoetryBot
 {
-	public static int POETRYBOTVERSION = 1;
+	public static String[] wordList;
 	
-	public void loadWords()
+	public static int POETRYBOTVERSION = 1;
+	public static String WORDLISTFILENAME = "brit-shuffle1.txt";
+	
+	public static void loadWords()
 	{
-		System.out.println("Begin loading words...");
+		System.out.println("Begin loading words from " + WORDLISTFILENAME + "...");
+		
+		List<String> wordArray = new LinkedList<String>();
 
 		BufferedReader reader = null;
 
@@ -29,11 +40,11 @@ public class PoetryBot
 		{
 			String currentLine;
 
-			reader = new BufferedReader(new FileReader("testfile.txt"));
-
+			reader = new BufferedReader(new FileReader(WORDLISTFILENAME));
+			
 			while((currentLine = reader.readLine()) != null)
 			{
-				System.out.println(currentLine);
+				wordArray.add(currentLine);
 			}
 
 		}
@@ -53,7 +64,16 @@ public class PoetryBot
 			}
 		}
 		
-		System.out.println("Done!");
+		wordList = new String[wordArray.size()];
+
+		System.out.println("Doing stuff. I'd like to put a progress bar here, but java won't let me. :(");
+		for(int i = 0; i < wordArray.size(); ++ i)
+		{
+			wordList[i] = wordArray.get(i);
+		}
+		
+		System.out.println("Done! Loaded " + wordList.length + " words!");
+		System.out.println();
 	}
 	
 	public void sayHello()
@@ -63,17 +83,23 @@ public class PoetryBot
 		System.out.println();
 	}
 
-	public void writePoem()
+	public void writePoem(String input)
 	{
-		System.out.print("What would you like me to write about? ");
-		Scanner listener = new Scanner(System.in);
-		String input = listener.nextLine();
-		listener.close();
-		System.out.println();
 		
 		System.out.println("I will write about " + '"' + input + '"' + ".");
 		
+		int wordIndex = getWordIndex(input);
+		
 		byte[] hash = getSHA512(input);
+		
+		if(wordIndex == -1)
+		{
+			System.out.println("I don't know what a " + '"' + input + '"' + " is, but I'll try my best.");
+			
+			wordIndex = generateWordIndex(input);
+		}
+		
+		System.out.println("Word index: " + wordIndex);
 		
 		BitSet hashBits = getBits(hash);
 		
@@ -85,6 +111,32 @@ public class PoetryBot
 		{
 			
 		}
+	}
+	
+	public static int generateWordIndex(String input)
+	{
+		int wordIndex = 1;
+		while(wordIndex < wordList.length)
+		{
+			wordIndex *= input.length();
+		}
+		
+		wordIndex = wordIndex % wordList.length;
+		
+		return wordIndex;
+	}
+	
+	public static int getWordIndex(String word)
+	{
+		for(int i = 0; i < wordList.length; ++ i)
+		{
+			if(wordList[i].equals(word))
+			{
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 	
 	private static BitSet[] getLines(BitSet lineChooser, BitSet hashBits)
@@ -159,9 +211,55 @@ public class PoetryBot
 	
 	public static void main(String[] args)
 	{
+		PoetryBot.loadWords();
 		PoetryBot pb = new PoetryBot();
+		
+		// Only need to run this once
+		//PoetryBot.shuffleWordsAndWriteToFile();
+		
 		pb.sayHello();
-		pb.loadWords();
-		pb.writePoem();
+		Scanner listener = new Scanner(System.in);
+		
+		boolean run = true;
+		
+		while(run)
+		{
+
+			System.out.print("What would you like me to write about? ");
+			String input = listener.nextLine();
+			System.out.println();
+			
+			pb.writePoem(input);
+			System.out.println();
+		}
+		listener.close();
+	}
+	
+	public static void shuffleWordsAndWriteToFile()
+	{
+		List<String> fooList = Arrays.asList(wordList);
+		
+		Collections.shuffle(fooList);
+		
+		PrintWriter writer =
+		null;
+		try
+		{
+			writer = new PrintWriter("brit-shuffle1.txt", "UTF-8");
+			
+			for(String s : fooList)
+			{
+				writer.println(s);
+			}
+		} catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		writer.close();
 	}
 }
